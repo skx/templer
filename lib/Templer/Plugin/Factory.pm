1f1e67efbb3c01689097dab39e8483e076b26c7d
Steve Kemp <steve@steve.org.uk>
2013-01-05 09:27:45 +0000

    Added POD

--- lib/Templer/Plugin/Factory.pm
+++ lib/Templer/Plugin/Factory.pm
@@ -1,10 +1,78 @@
-#
-#  This is a trivial plugin-factory, implemented as a singleton, which
-# allows plugins to register themselves by name.
-#
+
+=head1 NAME
+
+Templer::Plugin::Factory - A simple plugin class
+
+=cut
+
+=head1 DESCRIPTION
+
+=cut
+
+
+=head1 DESCRIPTION
+
+This class implements a singleton within which plugin classes may
+be registered and retrieved.
+
+The plugins used by C<templer> are of two forms:
+
+=over 8
+
+=item formatters
+
+These plugins operate upon the text contained in L<Templer::Site::Page> objects
+and transform the input into HTML.
+
+=item variable expanders
+
+These plugins, also operating on L<Templer::Site::Page> objects, are allowed
+the opportunity to modify, update, replace, or delete the various per-page
+variables.
+
+=back
+
+Plugins of each type register themselves by calling the appropriate methods
+in this class.
+
+=cut
+
+=head1 AUTHOR
+
+Steve Kemp <steve@steve.org.uk>
+
+=cut
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012-2013 Steve Kemp <steve@steve.org.uk>.
+
+This library is free software. You can modify and or distribute it under
+the same terms as Perl itself.
+
+=cut
+
+=head1 METHODS
+
+=cut
+
+
+
 package Templer::Plugin::Factory;
 
 my $singleton;
+
+
+
+
+=head2 new
+
+Constructor.
+
+This class is a singleton, so this method will either construct
+an instance of this class or return the global instance.
+
+=cut
 
 sub new
 {
@@ -14,9 +82,12 @@ sub new
 
 
 
-#
-# Load plugins from a given directory
-#
+=head2 load_plugins
+
+This method loads "*.pm" from the given directory.
+
+=cut
+
 sub load_plugins
 {
     my ( $self, $directory ) = (@_);
@@ -30,9 +101,18 @@ sub load_plugins
 
 
 
-#
-# Register a new formatter plugin.
-#
+=head2 register_formatter
+
+This method should be called by all formatting plugins to register
+themselves.  The two arguments are the name of the input-format,
+and the class-name which may be instantiated to process that kind
+of input.
+
+L<Templer::Plugin::Textile> and L<Templer::Plugin::Markdown> are
+example classes.
+
+=cut
+
 sub register_formatter
 {
     my ( $self, $name, $obj ) = (@_);
@@ -43,9 +123,17 @@ sub register_formatter
 }
 
 
-#
-# Register a new plugin for variable expansion.
-#
+=head2 register_formatter
+
+This method should be called by all variable-expanding plugins to register
+themselves.  The expected argument is the class-name which may be instantiated
+to expand variables.
+
+L<Templer::Plugin::ShellCommand>, L<Templer::Plugin::FileGlob>, and
+L<Templer::Plugin::FileContents> are examples of such plugins.
+
+=cut
+
 sub register_plugin
 {
     my ( $self, $obj ) = (@_);
@@ -55,11 +143,12 @@ sub register_plugin
 
 
 
-#
-#  Expand variables via all loaded plugins.
-#
-#  This is chained.
-#
+=head2 expand_variables
+
+Expand variables via all loaded plugins.
+
+=cut
+
 sub expand_variables
 {
     my ( $self, $page, $data ) = (@_);
